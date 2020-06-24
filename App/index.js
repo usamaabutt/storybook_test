d7f5ee08a32d514bbb1862c38cc61eee4eafd0a8
usamabutt <usamaabut081@gmail.com>
2020-06-24 23:08:52 +0500

    first commit

---
+++ App/index.js
@@ -0,0 +1,36 @@
+
+
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+// import React, {PureComponent} from 'react';
+// import {
+//   View,
+//   Text,
+// } from 'react-native';
+//
+//
+// export default class CardFormScreen extends PureComponent {
+//   constructor(props){
+//     super(props);
+//       this.state = {
+//     }
+//   }
+//
+//   render() {
+//
+//     return (
+//       <View style={{flex:1,justifyContent: 'center', alignItems: 'center'}}>
+//           <Text>Storybook</Text>
+//       </View>
+//     )
+//   }
+// }
+
+export default from '../storybook';
+
